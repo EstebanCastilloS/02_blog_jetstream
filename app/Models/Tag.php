454d4d169ebee3146b233ba3837ceee094d6af77
EstebanCastillo <ncastillo308@unab.edu.co>
2024-03-14 14:24:03 -0500

    Creando las relaciones Eloquents de los modelos

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    //relacion mucho a muchos polimorfica Inversa con post
+    public function posts(){
+        return $this->morphedByMany(Post::class, 'taggable');
+    }
+
 }
