c03fb243b173a7b3b92ee9d5ce98a2e3866031a6
EstebanCastillo <ncastillo308@unab.edu.co>
2024-03-17 23:33:41 -0500

    Crear mutadores Posts image()

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Post extends Model
 {
@@ -30,6 +31,56 @@ class Post extends Model
 
     protected $guarded = ['id'];
 
+    protected function title(): Attribute
+    {
+        return new Attribute(
+            set: fn ($value) => strtolower($value),
+            get: fn ($value) => ucfirst($value)
+        );
+    }
+
+    protected function image(): Attribute
+    {
+        return new Attribute(
+
+            get: fn() => $this->image_path
+            // get: fn() => $this->image_path ?? 'https://img.freepik.com/vector-premium/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093.jpg'
+
+            // get: function() {
+            //     if($this->image_path){
+
+            //         if(substr($this->image_path, 0, 8) === 'https'){
+            //             // dd($this->image_path);
+            //             return $this->image_path;
+            //         }
+
+            //         // dd(Storage::url($this->image_path));
+            //         return Storage::url($this->image_path);
+            //     }else{
+            //         return 'https://img.freepik.com/vector-premium/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093.jpg';
+            //     }
+            // }
+
+            // get: function() {
+            //     if($this->image_path){
+
+            //         if(substr($this->image_path, 0, 8) === 'https'){
+            //             return $this->image_path;
+            //         }
+
+            //         if (Storage::exists($this->image_path)) {
+            //             return Storage::url($this->image_path);
+            //         }
+            //     }
+
+            //     return 'https://img.freepik.com/vector-premium/vector-icono-imagen-predeterminado-pagina-imagen-faltante-diseno-sitio-web-o-aplicacion-movil-no-hay-foto-disponible_87543-11093.jpg';
+            // }
+
+
+
+
+        );
+    }
 
     //relacion uno a muchos inversa con usuarios
     public function user(){
