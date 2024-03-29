9e9784dfb9605fab83d3695f44cbb88babf8ec38
EstebanCastillo <ncastillo308@unab.edu.co>
2024-03-29 15:01:43 -0500

    Iniciando permisos en el Posts

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -22,9 +22,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('admin', function($user){
-            return $user->is_admin;
-        });
+        // Gate::define('admin', function($user){
+        //     return $user->is_admin;
+        // });
 
         // Gate::define('author', function($user, $post){
         //     return $user->id == $post->user_id;
